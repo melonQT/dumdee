684de90717b6dad29d9b9c031d9d83181f39298f
melonQT <129608612+melonQT@users.noreply.github.com>
2023-07-18 13:15:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,28 @@ def start_command(client, message):
 @app.on_message(filters.command("kick"))
 def kick(client, message):
     chat_id = message.chat.id
-    user_id = message.text.split()[1]
+
+    # Check if the command is replied to a user
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    elif len(message.command) > 1:
+        input_id = message.command[1]
+
+        # Check if the input is @username
+        if input_id.startswith("@"):
+            username = input_id[1:]
+            user = app.get_users(username)
+            if user:
+                user_id = user.id
+            else:
+                message.reply_text("Invalid @username.")
+                return
+        else:
+            # Assume input_id is user ID
+            user_id = input_id
+    else:
+        message.reply_text("Please reply to the user or provide a valid user ID.")
+        return
 
     try:
         app.ban_chat_member(chat_id, user_id)
